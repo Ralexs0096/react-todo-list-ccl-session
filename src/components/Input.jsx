781161a4db48a3758be546fb598403a5b380fd3f
Alexs Ruiz <ralexs.acu@gmail.com>
2025-04-16 19:51:18 -0600

    Remove state and component logic
    - previous change to uncontrolled component example

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,37 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Children from './Children';
 
 const Input = () => {
-  const [input, setInput] = useState({
-    input: '',
-    inputTwo: ''
-  });
-
-  const handleChange = (e) => {
-    // console.log({ name: e.target.name }, { value: e.target.value });
-    setInput({
-      ...input,
-      [e.target.name]: e.target.value
-    });
-  };
-
   return (
     <>
       <div className="input-container">
         <input
           className="input"
-          value={input.input}
           name="input"
-          onChange={handleChange}
-          type="text"
-          placeholder="Insert a TO-DO"
-        />
-
-        <input
-          className="input"
-          value={input.inputTwo}
-          name="inputTwo"
-          onChange={handleChange}
           type="text"
           placeholder="Insert a TO-DO"
         />
