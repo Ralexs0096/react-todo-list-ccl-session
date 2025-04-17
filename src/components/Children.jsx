22b24adb81fc90e47b7d2099335b097ed4501987
Alexs Ruiz <ralexs.acu@gmail.com>
2025-04-16 20:42:05 -0600

    useEffect cleanup function example

--- src/components/Children.jsx
+++ src/components/Children.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Children = () => {
+  useEffect(() => {
+    console.log('Children component mounted');
+
+    return () => {
+      // cleanup function
+      console.log('Component unmounted');
+    };
+  }, []);
+
   return <div>Children</div>;
 };
 
